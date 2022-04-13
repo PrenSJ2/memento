1253791d9fa95ac73edea90fad27961daef576a1
PrenSJ2 <seb.prentice@hotmail.com>
2022-04-13 16:55:30 +0100

    vercel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  { useState, useEffect } from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import shuffle from './utilities/shuffle';
 
 function App() {
+  const [cards, setCards] = useState(shuffle); // Cards array from assets
+  const [pickOne, setPickOne] = useState(null); // First Selection
+  const [pickTwo, setPickTwo] = useState(null); // Second Selection
+  const [disabled, setDisabled] = useState(false); // Delay handler
+  const [wins, setWins] = useState(0); //number of wins
+
+  // Handle card selection
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  };
+
+  // Start Over
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
+  };
+
+
+  // Used for selection and match handling
+  useEffect(() => {
+    let pickTimer;
+
+    // Two cards have been clicked
+    if (pickOne && pickTwo) {
+
+      // Check if the cards are the same
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image){
+              //Update card property to reflect match
+              return {...card, matched: true};
+            } else {
+              // no match
+              return card;
+            }
+          })
+        });
+        handleTurn();
+      } else {
+        // prevent new selections until after delay
+        setDisabled(true);
+        // add delay between selecctions
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo]);
+
+  // if player has found all matches
+  useEffect(() => {
+    // check for any remaining card matches
+    const checkWin = cards.filter((card) => !card.matched);
+
+    // all matches made, handle win/badge counters
+    if(cards.length && checkWin.length < 1) {
+      console.log('You Win!');
+      setWins(wins+1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins]);
+  
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header handleNewGame={handleNewGame} wins={wins} />
+      <div className='grid'>
+        {cards.map((card) => {
+          const {image, id, matched} = card;
+
+          return (
+            <Card 
+              key={id}
+              image={image}
+              selected={ card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
+            
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
