bea54d1408ea36d9b5f17e21e358dea412053150
PrenSJ2 <seb.prentice@hotmail.com>
2022-04-13 17:13:28 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './components/Card';
-import Header from './components/Header';
 import shuffle from './utilities/shuffle';
+
+
 
 function App() {
   const [cards, setCards] = useState(shuffle); // Cards array from assets
-  const [pickOne, setPickOne] = useState(null); // First Selection
-  const [pickTwo, setPickTwo] = useState(null); // Second Selection
+  const [pickOne, setPickOne] = useState(null); // First selection
+  const [pickTwo, setPickTwo] = useState(null); // Second selection
   const [disabled, setDisabled] = useState(false); // Delay handler
-  const [wins, setWins] = useState(0); //number of wins
+  const [wins, setWins] = useState(0); // Win streak
 
-  // Handle card selection
-  const handleClick = (card) => {
+   // Handle card selection
+   const handleClick = (card) => {
     if (!disabled) {
       pickOne ? setPickTwo(card) : setPickOne(card);
     }
@@ -23,39 +24,30 @@ function App() {
     setDisabled(false);
   };
 
-  // Start Over
-  const handleNewGame = () => {
-    setWins(0);
-    handleTurn();
-    setCards(shuffle);
-  };
-
-
   // Used for selection and match handling
   useEffect(() => {
     let pickTimer;
 
     // Two cards have been clicked
     if (pickOne && pickTwo) {
-
-      // Check if the cards are the same
+      // Check if the cards the same
       if (pickOne.image === pickTwo.image) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
-            if (card.image === pickOne.image){
-              //Update card property to reflect match
-              return {...card, matched: true};
+            if (card.image === pickOne.image) {
+              // Update card property to reflect match
+              return { ...card, matched: true };
             } else {
-              // no match
+              // No match
               return card;
             }
-          })
+          });
         });
         handleTurn();
       } else {
-        // prevent new selections until after delay
+        // Prevent new selections until after delay
         setDisabled(true);
-        // add delay between selecctions
+        // Add delay between selections
         pickTimer = setTimeout(() => {
           handleTurn();
         }, 1000);
@@ -67,38 +59,34 @@ function App() {
     };
   }, [cards, pickOne, pickTwo]);
 
-  // if player has found all matches
-  useEffect(() => {
-    // check for any remaining card matches
-    const checkWin = cards.filter((card) => !card.matched);
-
-    // all matches made, handle win/badge counters
-    if(cards.length && checkWin.length < 1) {
-      console.log('You Win!');
-      setWins(wins+1);
-      handleTurn();
-      setCards(shuffle);
-    }
-  }, [cards, wins]);
+    // If player has found all matches, handle accordingly
+    useEffect(() => {
+      // Check for any remaining card matches
+      const checkWin = cards.filter((card) => !card.matched);
   
-
-
+      // All matches made, handle win/badge counters
+      if (cards.length && checkWin.length < 1) {
+        console.log('You win!');
+        setWins(wins + 1);
+        handleTurn();
+        setCards(shuffle);
+      }
+    }, [cards, wins]);
 
 
   return (
     <>
-      <Header handleNewGame={handleNewGame} wins={wins} />
-      <div className='grid'>
-        {cards.map((card) => {
-          const {image, id, matched} = card;
 
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+          
           return (
-            <Card 
+            <Card
               key={id}
               image={image}
-              selected={ card === pickOne || card === pickTwo || matched}
+              selected={card === pickOne || card === pickTwo || matched}
               onClick={() => handleClick(card)}
-            
             />
           );
         })}
